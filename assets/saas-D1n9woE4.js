b60d824a6141c9df21b546f532c93a52291e25ba
markegidiDev <markegidiDev@users.noreply.github.com>
2026-09-14 16:06:46 +0000

    deploy: 2b83332171dd0ac9bdd43db776c220615bb99d47

--- assets/saas-D1n9woE4.js
+++ assets/saas-D1n9woE4.js
@@ -1,4 +1,4 @@
-import{al as Fr,aE as ui,an as ue,r as k,am as Q,R as P,ar as Mw,ao as ye,ap as It,aF as Cw}from"./index-CSGC6D6-.js";/**
+import{al as Fr,aE as ui,an as ue,r as k,am as Q,R as P,ar as Mw,ao as ye,ap as It,aF as Cw}from"./index-4ZidqHEX.js";/**
  * @license lucide-react v0.510.0 - ISC
  *
  * This source code is licensed under the ISC license.
